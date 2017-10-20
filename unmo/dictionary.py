dd2ad58f1480b029476388fd999a857384953351
sandmark <sandmark.m@gmail.com>
2017-10-20 23:17:32 +0900

    Add docstring for `dicfile`

--- unmo/dictionary.py
+++ unmo/dictionary.py
@@ -17,6 +17,7 @@ class Dictionary:
     スタティックメソッド:
     line2pattern(str) -- パターン辞書読み込み用のヘルパー
     pattern2line(pattern) -- パターンハッシュをパターン辞書形式に変換する
+    dicfile(key) -- keyで指定された辞書のフルパスを返す
 
     load_random(file) -- fileからランダム辞書の読み込みを行う
     load_pattern(file) -- fileからパターン辞書の読み込みを行う
